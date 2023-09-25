f294ef7c9638b2f04de5d3c8cc0c276ed5666e78
Dustysy <112944489+Dustysy@users.noreply.github.com>
2023-09-25 20:49:55 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+  <section class="reviews">
+  <div class="wrapper">
+    <div class="reviews__wrapper">
+      <div class="comment">
+        <div class="review__image-wrapper">
+          <img src="photo-1.jpg" width="50" height="50">
+        </div>
+        <span>Иван</span>
+        <p>Очень самобытный и необычный проект.</p>
+      </div>
+
+      <div class="comment">
+        <div class="review__image-wrapper">
+          <img src="photo-3.jpg" width="50" height="50">
+        </div>
+        <span>Мария</span>
+        <p>Мне особенно понравилось решение лестничных клеток. 
+          <br>Кружки вообще очень стильные!</p>
+      </div>
+
+      <div class="comment">
+        <div class="review__image-wrapper">
+          <img src="photo-2.jpg" width="50" height="50">
+        </div>
+        <span>Светлана</span>
+        <p>Очень нравится, что в городе развиваются такие проекты. Надеюсь, такие проекты скоро будут не только в <br>частных домах, но и в кафе, музеях, театрах.
+          <br>Я бы сходила!</p>
+      </div>
+    </div>
+  </div>
+  </section>
+?>
---
+++ header.php
@@ -0,0 +1,55 @@
+<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;1,200&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>INTERIOR LOUNGE — дизайн общественных пространств</title>
+</head>
+<body>
+  <header>
+		<div class="wrapper">
+			<div class="page-header__wrapper">
+				<div class="page-header__logo-wrapper">
+					<a href="#">
+						<img src="logo.svg" width="262" height="47" alt="INTERIOR LOUNGE — дизайн общественных пространств">
+					</a>
+				</div>
+				<div class="page-header__menu-wrapper">
+					<nav class="page-header__navigation">
+						<div class="menu-primary-container">
+							<ul class="menu">
+								<li class="menu-item menu-item-has-children">
+									<a href="#">Наши проекты</a>
+									<ul class="sub-menu">
+										<li class="menu-item">
+											<a href="#">Tomazzi Hall</a>
+										</li>
+
+										<li class="menu-item">
+											<a href="#">Lounge Hall</a>
+										</li>
+
+										<li class="menu-item">
+											<a href="#">Astra Hall</a>
+										</li>
+									</ul>
+								</li>
+								<li class="menu-item current-menu-item">
+									<a href="#">Контакты</a>
+								</li>
+								<li class="menu-item menu-item-styled">
+									<a href="#">Оставить отзыв</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+  </header>
+?>
